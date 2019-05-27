7ea9f7f3a155578798b365c5fb4ba6355bbdd750
mcamp <usmcamp0811@gmail.com>
2019-05-26 20:56:15 -0500

    added search page.. still needs work.. learning more

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"></Todos>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Todos from './components/Todos';
+import Header from './components/layout/header.vue';
+import AddTodo from './components/AddTodo.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      axios.delete('https://jsonplaceholder.typicode.com/todos/${id}')
+        .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+        .catch(err => console.log(err));
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, newTodo])
+        .catch(err => console.log(err));
+    }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
+  }
+}
+</script>
+
+<style>
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fffff;
+  margin-top: 60px;
+
+}
+
+body {
+    margin: 0;
+    padding: 0;
+    background-color:#2c3e50;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
+</style>
